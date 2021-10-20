b76b9dd73b2cabdf12813223407f61592dde3247
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2021-10-20 16:23:40 +0200

    Configurando os usuario (username, password, role) e configurando uma memoria para armazenar os usuarios.

--- src/main/java/com/mafurrasoft/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/mafurrasoft/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.mafurrasoft.springsecurity.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -19,5 +24,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails eacuamba = User.builder()
+                .username("eacuamba")
+                .password("senha")
+                .roles("ESTUDANTE")
+                .build();
+
+        return new InMemoryUserDetailsManager(eacuamba);
     }
 }
