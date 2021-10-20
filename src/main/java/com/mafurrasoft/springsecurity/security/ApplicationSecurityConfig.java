252b79de9cb01057f342ab46d1579a21d18e0aed
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2021-10-20 16:55:54 +0200

    Configurando o PasswordEncoder, responsavel por encriptar o senha e validar, depois encriptei a senha que havia definido.

--- src/main/java/com/mafurrasoft/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/mafurrasoft/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.mafurrasoft.springsecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,11 +9,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 //        super.configure(http);
@@ -31,7 +41,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected UserDetailsService userDetailsService() {
         UserDetails eacuamba = User.builder()
                 .username("eacuamba")
-                .password("senha")
+                .password(this.passwordEncoder.encode("senha"))
                 .roles("ESTUDANTE")
                 .build();
 
