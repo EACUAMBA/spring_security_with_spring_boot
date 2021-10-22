1461bae2989b3d3052edcc4d15e4245eeac7be6e
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2021-10-22 18:24:06 +0200

    Adicionando o controller de gerenciamento de estudantes e cursos.

---
+++ src/main/java/com/mafurrasoft/springsecurity/controller/managment/StudentController.java
@@ -0,0 +1,53 @@
+package com.mafurrasoft.springsecurity.controller.managment;
+
+import com.mafurrasoft.springsecurity.domain.Student;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController(value = "managementStudentController")
+@RequestMapping(path = "management/api/v1/students")
+public class  StudentController {
+
+    private static final List<Student> STUDENT_LIST = Arrays.asList(
+            new Student(1, "Jorge Himbu"),
+            new Student(2, "Rodrigo Kumba"),
+            new Student(3, "Ilurio Plunito"),
+            new Student(4, "Gutyo Kilamba")
+    );
+
+    @GetMapping
+    public List<Student> getStudentList(){
+        System.out.println("getStudentList");
+        return STUDENT_LIST;
+    }
+
+    @GetMapping(path = "{studentId}")
+    public void print_A_Student(@PathVariable("studentId") Integer studentId){
+        System.out.println("print_A_Student");
+        System.out.println(STUDENT_LIST.stream()
+                .filter((student)->{return student.getId().equals(studentId);})
+                .findFirst()
+                .orElseThrow(()-> new IllegalStateException("Student " + studentId +" does not exists.")));
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student){
+        System.out.println("registerNewStudent");
+        System.out.println(student);
+    }
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student){
+        System.out.println("updateStudent");
+        System.out.println(String.format("%nID - %d,%n New Student Instance: %s", studentId, student));
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Integer studentId){
+        System.out.println("deleteStudent");
+        System.out.println("StudentId: " + studentId);
+    }
+
+
+}
