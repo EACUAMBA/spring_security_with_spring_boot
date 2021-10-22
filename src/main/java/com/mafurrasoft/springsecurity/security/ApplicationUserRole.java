64cdbd97e6e203e0146ca889449ff38e3dccf471
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2021-10-22 18:55:47 +0200

    Adicionando permissões dentro das roles, agora podemos saber as permissões que uma role tem.
    Para isso estamso criando uma lista (set) de SimpleGrandAuthority, vamos percorer as roles e vamos mapear cada role para um objecto SimpleGrandAuthority que recebe como parametro uma String.
    Apos isso vamos nessa lista, adicionar a role do usuario, concatenamos a role com ROLE_ nome da role.

--- src/main/java/com/mafurrasoft/springsecurity/security/ApplicationUserRole.java
+++ src/main/java/com/mafurrasoft/springsecurity/security/ApplicationUserRole.java
@@ -1,9 +1,12 @@
 package com.mafurrasoft.springsecurity.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.mafurrasoft.springsecurity.security.ApplicationUserPermission.*;
 
@@ -20,5 +23,17 @@ public enum ApplicationUserRole {
 
     public Set<ApplicationUserPermission> getPermissionSet() {
         return permissionSet;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthoritySet(){
+        Set<SimpleGrantedAuthority> permissions = getPermissionSet().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+
+        permissions.add(new SimpleGrantedAuthority("ROLE_"+this.name()));
+
+        return permissions;
+
+
     }
 }
