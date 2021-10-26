bf3d8e603b3fead4cf0bbbe7bbccafb4477dcf23
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2021-10-26 20:55:37 +0200

    Fazendo o spring não esconder o CSRF e gerar ele forçosamente.

--- src/main/java/com/mafurrasoft/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/mafurrasoft/springsecurity/security/ApplicationSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static com.mafurrasoft.springsecurity.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.mafurrasoft.springsecurity.security.ApplicationUserPermission.STUDENT_WRITE;
@@ -34,7 +35,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 //        super.configure(http);
         http
-                .csrf().disable()
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
