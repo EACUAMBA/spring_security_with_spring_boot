07e49deb6229e828f97432376fe0317963042f88
Edilson Alexandre Cuamba <EdilsonCuamba@gmail.com>
2021-11-12 23:14:22 +0200

    Habilitando o recurso remember-me, que permite que alguém fique online em uma máquina durante um tempo especifico, que é por padrão de 2 semanas.

--- src/main/java/com/mafurrasoft/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/mafurrasoft/springsecurity/security/ApplicationSecurityConfig.java
@@ -50,7 +50,9 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login").permitAll()
-                .defaultSuccessUrl("/courses", true);
+                .defaultSuccessUrl("/courses", true)
+                .and()
+                .rememberMe();
     }
 
     @Override
