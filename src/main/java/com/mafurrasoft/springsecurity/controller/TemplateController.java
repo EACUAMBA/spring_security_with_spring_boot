e9f72c4155890bebff29a03a3a20c1f31f0999ed
Edilson Alexandre Cuamba <EdilsonCuamba@gmail.com>
2021-11-12 22:45:26 +0200

    Criando a view e criando o controller da view, sendo que para retornarmos a view so precisamos de retornar uma string no controller com o nome do template.

---
+++ src/main/java/com/mafurrasoft/springsecurity/controller/TemplateController.java
@@ -0,0 +1,14 @@
+package com.mafurrasoft.springsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/")
+public class TemplateController {
+    @GetMapping(path = "/login")
+    public String getLoginView(){
+        return "login";
+    }
+}
