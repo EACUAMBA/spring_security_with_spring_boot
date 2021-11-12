364a1b8dcad0ec718900013c9fd75b959db710d7
Edilson Alexandre Cuamba <EdilsonCuamba@gmail.com>
2021-11-13 00:32:49 +0200

    Adicionando metodo de logout e limpamdo a sessão o cache e apagando os cookies, o log out por padrão funciona com o CSRF ligado onde só aceita request com o metodo POST para segurança, mas comom desabilitamos o CSRF podemos sem problema fazer o logout com um simples mettodo GET.
    Quando habiilitamos o CSRF não podemos mais especificar que tipode de metodo queremos para logout, ou um deles. se habilitarmos csrf e deixarmos está linha de codigo estaremos permtindo logout usando metodo POST.

--- src/main/java/com/mafurrasoft/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/mafurrasoft/springsecurity/security/ApplicationSecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 import java.util.concurrent.TimeUnit;
 
 import static com.mafurrasoft.springsecurity.security.ApplicationUserPermission.COURSE_WRITE;
@@ -54,8 +56,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/courses", true)
                 .and()
                 .rememberMe()
-                    .tokenValiditySeconds((int)TimeUnit.DAYS.toMillis(30))
-                    .key("algomuitoseguro");
+                .tokenValiditySeconds((int)TimeUnit.DAYS.toMillis(30))
+                .key("algomuitoseguro")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID", "remember-me")
+                .logoutSuccessUrl("/login");
+
     }
 
     @Override
