7c8747f152d1acd2962fb3ad71a312fa8054661a
Edilson Alexandre Cuamba <EdilsonCuamba@gmail.com>
2021-11-12 22:45:57 +0200

    Definindo a página de login programaticamente.

--- src/main/java/com/mafurrasoft/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/mafurrasoft/springsecurity/security/ApplicationSecurityConfig.java
@@ -48,7 +48,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("/login").permitAll();
     }
 
     @Override
